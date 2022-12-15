502391a4211e46ce5e61cdd404ea7ae9f768c0f5
irinaamsn <99390242+irinaamsn@users.noreply.github.com>
2022-12-15 17:45:49 +0300

    Update ClientService.cs

--- CRMModel/Модель/ClientService.cs
+++ CRMModel/Модель/ClientService.cs
@@ -1,256 +1,197 @@
 using ImitModelBl.Model;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Metrics;
+using System.ComponentModel;
 using System.Linq;
-using System.Runtime.ConstrainedExecution;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Linq;
-using static ImitModelBl.Model.Category;
 using static ImitModelBl.Model.EnumPlaceSevices;
-using System.Timers;
-using Timer = System.Threading.Timer;
-using System.Collections;
+//using System.Timers;
 
 namespace ImitModelBl.Model
 {
-    public class ComputerModel
+    public class ClientService//обслуживание клиента
     {
-        Random rnd = new();
-        Generator generator = new();
-        bool isRunning = false;
-        //public List<Service> Services { get; set; } = new List<Service>();
-        // List<Check> Checks { get; set; } = new List<Check>();
-        //public List<Sell> Sell { get; set; } = new List<Sell>();
-        public Queue<Master> BelowMasters { get; set; } = new Queue<Master>();
-        public Queue<Master> HairMasters { get; set; } = new Queue<Master>();
-        public Queue<Master> ManMasters { get; set; } = new Queue<Master>();
-        public Queue<Master> SpaMasters { get; set; } = new Queue<Master>();
-        public Queue<Master> StyleMasters { get; set; } = new Queue<Master>();
-       // public List<ListServices> ListServices { get; set; } = new List<ListServices>();//1 cust(all list of services) = 1 master
-        public List<ClientService> ClientServices { get; set; } = new List<ClientService>();
-        public int CountWashingHair { get; set; } = 2;
-        public int CountHairDrying { get; set; } = 1;
-        public int CountHairChair { get; set; } = 3;
-        public int CountStylingChair { get; set; } = 3;
-        public int CountСhairSpa { get; set; } = 2;
-        public int CountManHairChair { get; set; } = 2;
-        public int CountPlaces ;
+        public int NumberPlaceOfService { get; set; }//number
+        public Master Master { get; set; }//мастер который обслуживает
+        public Queue<ListServices> Queue { get; set; }//очередь из пакета услуг каждого клиента (очередь клиентов)
+        public Queue<Service> QueueServices { get; set; } = new Queue<Service>();
+        public int TimeWorkingPlace { get; set; }
+        //public bool IsExit { get; set; }=false; 
+        public int ExitCustomer { get; set; }//не дождались в очереди//static
+        public int CustomerReadyCount { get; set; }//кол-во готовых клиентов, чек закрыт?//static
+        public int Count => QueueServices.Count;//кол-во
+        public List<Customer> ListCustomerReady { get; set; } = new List<Customer>();//
+        public int TimeWashingHair { get; set; } = 20;
+        public int TimeHairDrying { get; set; } = 15;
+       
 
+        public event EventHandler<Check> CheckClosed;//событие того что клиент обслужился
+        //AutoResetEvent e v = new AutoResetEvent(false);
 
-        AutoResetEvent autoEvent = new AutoResetEvent(false);
-        
-        public ComputerModel()
+        public ClientService(int number,Master master)
         {
-            var freeMasters = generator.GeneratorMasters(3,2,2,3,3);//TODO//WinForm
-             generator.GeneratorServices();//TODO
-                                           //var customers = generator.GeneratorNewCustomers(13);//TODO???????
-            CountPlaces = CountWashingHair + CountHairDrying + CountHairChair + CountСhairSpa + CountManHairChair;
+            NumberPlaceOfService = number;
+            Master = master;
+            Queue = new Queue<ListServices>();//СПИСОК СВОИХ КЛИЕНТОВ//////TODO
+            //TimeWorkingPlace = Queue.FirstOrDefault().SumTime;
+        }
 
-            foreach (var master in freeMasters.Where(m=> m.Speciality== CategoryMaster.Hairdresser.ToString()))
+        public async void WaitingCustomer(Customer customer)//метод ожидания клиента своей услуги
+        {
+            var ts = TimeSpan.FromMilliseconds(customer.TimeWait * 10);
+            
+            await Task.Delay(ts);
+            //if (t.Wait(ts))
             {
-                HairMasters.Enqueue(master);
+                if (!customer.IsCustomerReady)
+                {
+                    // foreach(var cust in Queue)
+                    //if (Queue.Contains(customer.ListServices)
+                    Queue.Dequeue();
+                    customer.IsCustomerReady = false;
+<<<<<<< HEAD:CRMModel/Model/ClientService.cs
+                   // Queue.Dequeue();    
+=======
+                       
+>>>>>>> 9521deaf84a12c6ddb2696789f911ff723e6a785:CRMModel/Модель/ClientService.cs
+                    ExitCustomer++;
+                    //foreach(Service service in exitCustomer)
+                    //{
+                    //    //var serv = QueueServices.Where(x => x.ServiceId == service.ServiceId);
+                    //    QueueServices.Dequeue();
+                    //}
+                }
             }
+            
+        } 
+        //public List<Service>
+        public void EnqueueService(Service service)
+        {
 
-            foreach (var master in freeMasters.Where(m => m.Speciality == CategoryMaster.ManMaster.ToString()))
+            QueueServices.Enqueue(service);
+           // ListServices.Add(service);
+        }
+        public async void WaitingClientService(Service service, string namePlace)
+        {
+            
+            if (QueueServices.Count >0)
             {
-                ManMasters.Enqueue(master);
+                int timeService = service.TimeRunning;//TODO
+                if ((int)Enum.Parse(typeof(PlaceServices), namePlace) == 1) timeService = TimeHairDrying;
+                if ((int)Enum.Parse(typeof(PlaceServices), namePlace) == 2) timeService = TimeWashingHair;
+                var ts = TimeSpan.FromMilliseconds(timeService * 10);
+                
+                await Task.Delay(ts);
+                QueueServices.Dequeue();//убираем из очереди услугу
+            }
+            
+        }
+        public void Enqueue(ListServices listServices)
+        {
+            //if (Queue.Count < MaxQueueLenght)//TODO//change on while - TimeWait
+
+            Queue.Enqueue(listServices); //ставим в очередь клиента
+
+            WaitingCustomer(listServices.Customer);//ожилает
+            //Timer timer = new Timer();
+            //timer.Interval = customerTime * 100;
+            //timer.Elapsed += OnTimedEvent;
+            //timer.Enabled = true;
+
+            //var TTimer = new Timer(
+            //    new TimerCallback(TickTimer),
+            //    null,
+            //    1000,
+            //    TimeWorkingPlace);
+        }
+
+
+        //private void TickTimer(object state)
+        //{
+        //    AutoResetEvent autoEvent = (AutoResetEvent)state;
+
+        //    //if (!listServices.Customer.IsCustomerReady)
+        //    //{
+        //    //    ExitCustomer++;
+        //    //   var customerExit =  Queue.Dequeue();
+        //    //}
+        //    //return new TimerCallback(TickTimer(new ListServi);
+        //}
+
+        //private void TickTimer(object state, ListServices list)
+        //{
+           
+        //}
+        //private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        //{
+        //    ExitCustomer++;
+        //}
+
+        public decimal Dequeue()
+        {
+            decimal sum = 0;
+            if (Queue.Count == 0)
+            {
+                return 0;
+            }
+            
+            var listMasters = new List<Master>();
+            listMasters.Add(Master);
+
+           // ListServices list = null;
+            //delete from queue customer
+            var list = Queue.Dequeue();
+
+            if (list != null)
+            {
+                // var t = list.SumTime;
+                foreach (Service item in list)
+                {
+                    item.PlaceServiceType.ForEach(p => WaitingClientService(item,p));
+                }
+                //token//TODO
+                list.Customer.IsCustomerReady = true;
+                ListCustomerReady.Add(list.Customer);
+                CustomerReadyCount++;
+                var check = new Check()
+                {   //Master = Master,
+                    CheckId = list.Customer.CustomerId,
+                    Masters = listMasters,//???????/
+                    CustomerId = list.Customer.CustomerId,
+                    Customer = list.Customer
+                };
+
+                var sells = new List<Sell>();//список услуг клиента
+
+                foreach (Service service in list)
+                {
+                    var sell = new Sell()
+                    {
+
+                        Check = check,
+                        ServiceId = service.ServiceId,
+                        Service = service,
+                        MasterId = Master.MasterId,
+                        Master = Master
+                    };
+
+                    sells.Add(sell);
+                    sum += service.Price;//сумма за все оказанные услуги
+                }
+
+                check.Price = sum;//итоговая сумма на выход
+
+                var result = $"Check: CustomerId {check.CustomerId}, sum {sum}\n";
+
+                CheckClosed?.Invoke(this, check);//обслужился
                 
             }
-            foreach (var master in freeMasters.Where(m => m.Speciality == CategoryMaster.SpaMaster.ToString()))
-            {
-                SpaMasters.Enqueue(master);
-              
-            }
-            foreach (var master in freeMasters.Where(m => m.Speciality == CategoryMaster.Stylist.ToString()))
-            {
-                StyleMasters.Enqueue(master);
-               
-            }
-            foreach (var master in freeMasters.Where(m => m.Speciality == CategoryMaster.BelowHairdresser.ToString()))
-            {
-                BelowMasters.Enqueue(master);
-
-            }
-            //foreach(var master in freeMasters)
-            //{
-            //    Masters.Enqueue(master);
-            //}
-            int Id = 1;
-            while(CountPlaces>0)
-            {
-                for (int i = 0; i < CountHairChair; i++)////кол-во мест обслуживания TODO//WinForm
-                {
-                    ClientServices.Add(new ClientService(Id, HairMasters.Dequeue()));
-                    CountPlaces--;Id++;
-                    // ClientServices.Add(new ClientService((int)PlaceServices.HairDressingChair, ManMasters.Dequeue()));
-                }
-                for (int i = 0; i < CountHairDrying; i++)////кол-во мест обслуживания//TODO//WinForm
-                {
-                    ClientServices.Add(new ClientService(Id, BelowMasters.Dequeue())); CountPlaces--;Id++;
-                }
-                for (int i = 0; i < CountWashingHair; i++)////кол-во мест обслуживания
-                {
-                    ClientServices.Add(new ClientService(Id, BelowMasters.Dequeue())); CountPlaces--;Id++;
-                }
-                for (int i = 0; i < CountСhairSpa; i++)
-                {
-                    ClientServices.Add(new ClientService(Id, SpaMasters.Dequeue())); CountPlaces--; Id++;
-                }
-                for (int i = 0; i < CountStylingChair; i++)
-                {
-                    ClientServices.Add(new ClientService(Id, StyleMasters.Dequeue())); CountPlaces--; Id++;
-                }
-                for (int i = 0; i < CountManHairChair; i++)
-                {
-                    ClientServices.Add(new ClientService(Id, ManMasters.Dequeue())); CountPlaces--; Id++;
-                }
-                //for (int i= 0; i < 5; i++)////кол-во мест обслуживания= 5 услуг - TODO//WinForm
-                //{
-                //    ClientServices.Add(new ClientService(ClientServices.Count,Masters.Dequeue()));
-                //}
-            }
-
-        }
-
-        public void Start()//TODO//async
-        {
-            isRunning = true;
-
-            //var time = ClientServices.FirstOrDefault(X=>X.NumberPlaceOfService==ClientServices.Count).Queue.First(x => x == ListServices.Select(x => x.SumTime)).SumTime;
-            var taskCustomers =  Task.Run(() => CreateListServices(15));//TODO
-           // Thread.Sleep(5000);                                                          // taskCustomers.Wait();
-            autoEvent.WaitOne();
-           var clientServiceTask = ClientServices.Select(c => new Task(() =>  CashClientService(c)));
-
-            foreach(var task  in clientServiceTask)
-            {
-                task.Start();
-            }
-        }
-        public void Stop()
-        {
-            isRunning= false;   
+            return sum;
         }
        
-        public int Waiting = 0;
-        void CashClientService(ClientService clientService)//TODO// int TimeRunning
-        {
-            while(isRunning)
-            {
-               if (clientService.Count > 0)//TODO//кол-во обслуживающих мест//если очередь не пустпя то вызываем клиента из очереди
-                {
-                    // int t = clientService.TimeWorkingPlace;//TODO
-
-                    // var ts = TimeSpan.FromMilliseconds(t * 10);
-
-                    //await Task.Run(() =>
-                    // {
-                    //     Thread.Sleep(t *10);//время обслуживания
-                    // });
-                    // await task2;
-                    //if (task2.Wait(ts))
-                    //{
-
-                    //var service = clientService.QueueServices.Dequeue();
-                    //foreach (var pl in service.PlaceServiceType)
-                    //{
-                    //    //поиск кресла номер которого равен номеру места услуги которая выполняется 
-                    //   var place = ClientServices.Find(c => c.NumberPlaceOfService == (int)Enum.Parse(typeof(PlaceServices), pl));
-                    //    place.WaitingClientService(service, pl);
-                    //    // cl.WaitingClientService(service);//ставим услугу в ожидание
-                    //}
-                     clientService.Dequeue();
-                    //}
-                    //var t = list.SumTime;
-
-                    //var ts = TimeSpan.FromMilliseconds(t*10);
-
-                    //Task task = Task.Run(() => 
-                    //    Thread.Sleep(t * 10));//время обслуживания
-
-
-                    //var TTimer = new Timer( new TimerCallback(TickTimer(clientService)),                     
-                    //    null,
-                    //    50,
-                    //    Waiting);
-                    // autoEvent.WaitOne();
-
-                }
-            }
-        }
-
-        private TimerCallback TickTimer(ClientService clientService)
-        {
-            clientService.Dequeue();
-           // autoEvent.Set();
-            return new TimerCallback(TickTimer(clientService));
-        }
-
-        private void TickTimer(object state, ClientService clientService)
-        {
-            AutoResetEvent autoEvent = (AutoResetEvent)state;
-
-            //if (Waiting)
-        }
-
-        void CreateListServices(int countCustomers)//create customers//нормaльное распр
-        {
-           while (isRunning)
-            {
-                var customers = generator.GeneratorNewCustomers(countCustomers);
-                foreach (var customer in customers)
-                {
-                    //var newQueue = new Queue<Service>();
-                    ClientService cl = null;
-                    var listPlaces = new List<int>();
-                    //ClientService cash = null;
-                    var listService = new ListServices(customer);
-                    foreach (var service in generator.GetRandomServices(1, 3))
-                    {
-                        listService.Add(service);
-                        foreach (var pl in service.PlaceServiceType)
-                        {
-                            //поиск кресла номер которого равен номеру места услуги которая выполняется 
-                            var clArr = ClientServices.FindAll(c => c.NumberPlaceOfService == (int)Enum.Parse(typeof(PlaceServices), pl)).ToArray();//TODO
-                            var minCount = clArr.Min(x=>x.Count);
-                            cl = clArr.FirstOrDefault(x => x.Count == minCount);
-                            cl.EnqueueService(service);
-                            //Task.Run(() => CashClientService(c)
-                            listPlaces.Add(cl.NumberPlaceOfService);//id//TODO
-                           
-                            // cl.WaitingCustomer(customer);//ставим услугу в ожидание
-                            //cash = ClientServices.
-                        }
-
-                        //cl.Enqueue(listService);//ставим клиента полностью в ожилание пока мастера не начнут обслуживать его пакет услуг
-
-                    }
-
-                    foreach (var pl in listPlaces)
-                    {
-                        //поиск кресла номер которого равен номеру места услуги которая выполняется 
-                        
-                        // cl = ClientServices.Find(c => c.NumberPlaceOfService == (int)Enum.Parse(typeof(PlaceServices), pl));
-                        cl = ClientServices.SingleOrDefault(x => x.NumberPlaceOfService == pl);//enum//TODO
-                        cl.Enqueue(listService);
-
-                       // cl.WaitingCustomer(customer);//ставим услугу в ожидание
-                                                     //cash = ClientServices.
-                    }
-                    // var cash = ClientServices.FirstOrDefault(cl=>cl.NumberPlaceOfService==);//choose master random//TODO
-                    //cash.TimeWorkingPlace = listService.SumTime; 
-                    //cash.Enqueue(listService);
-
-                }
-               autoEvent.Set();
-               Thread.Sleep(2000);//TODO//time generator new customers
-            }
-
-          //TODO!!! выполняется обслуживается одно клиетнат(его пакета усулг) async и await параллельно с ругими клиентамит (без async/await) 
-           
-        }
     }
+    
 }
